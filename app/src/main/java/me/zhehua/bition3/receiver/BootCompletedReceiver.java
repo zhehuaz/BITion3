b90671120def2f69a0c7587d6db1f8da1ada0091
Zhehua Chang <zhehuaxiao@gmail.com>
2016-03-14 22:09:43 +0800

    Test service bind.

--- app/src/main/java/me/zhehua/bition3/receiver/BootCompletedReceiver.java
+++ app/src/main/java/me/zhehua/bition3/receiver/BootCompletedReceiver.java
@@ -10,9 +10,6 @@ import org.greenrobot.eventbus.EventBus;
 import me.zhehua.bition3.events.BootCompleteEvent;
 import me.zhehua.bition3.service.LoginService;
 
-/**
- * Created by Administrator on 2016/2/28.
- */
 public class BootCompletedReceiver extends BroadcastReceiver {
     public final static String TAG = "BootCompletedReceiver";
     @Override
@@ -20,6 +17,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         Log.i(TAG, intent.getAction() + " received");
         Intent serviceIntent = new Intent(context, LoginService.class);
         context.startService(serviceIntent);
-        EventBus.getDefault().post(new BootCompleteEvent());
+        //EventBus.getDefault().post(new BootCompleteEvent());
     }
 }
--- app/src/main/java/me/zhehua/bition3/ui/MainActivity.java
+++ app/src/main/java/me/zhehua/bition3/ui/MainActivity.java
@@ -1,39 +1,55 @@
 package me.zhehua.bition3.ui;
 
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Binder;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import me.zhehua.bition3.R;
+import me.zhehua.bition3.service.LoginService;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String TAG = "MainActivity";
+    ServiceConnection loginServiceConnection;
+    Service loginService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, LoginService.class);
+        startService(intent);
+        loginServiceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                Log.i(TAG, "onServiceConnected()");
+                loginService = ((LoginService.LoginServiceBinder) service).getService();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                Log.i(TAG, "onServiceDisconnected()");
+            }
+        };
+
+        bindService(intent, loginServiceConnection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    protected void onDestroy() {
+        super.onDestroy();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        unbindService(loginServiceConnection);
+        loginService = null;
     }
 }
